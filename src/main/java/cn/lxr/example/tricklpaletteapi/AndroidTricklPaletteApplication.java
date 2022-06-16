dc4bb61d1dd242a8a67ee892b33eaf4b69cb634b
刘相然[liuxiangran] <liuxiangran@xiaomi.com>
2022-06-16 17:52:11 +0800

    init commit

---
+++ src/main/java/cn/lxr/example/tricklpaletteapi/AndroidTricklPaletteApplication.java
@@ -0,0 +1,13 @@
+package cn.lxr.example.tricklpaletteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidTricklPaletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidTricklPaletteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lxr/example/tricklpaletteapi/AndroidTricklPaletteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lxr.example.tricklpaletteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidTricklPaletteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
